6b95e09279bdd42dc99a14d3c1065985dd461770
Larry Kyle <lknaron@asu.edu>
2016-02-17 22:37:05 -0600

    Move unit test files

---
+++ src/net/sf/memoranda/tests/AllTests.java
@@ -0,0 +1,14 @@
+package net.sf.memoranda.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalendarDateNextDayTest.class, 
+				CalendarDateTest.class, 
+				ContactTest.class, 
+				EventsManagerTest.class })
+public class AllTests {
+
+}
--- src/net/sf/memoranda/tests/CalendarDateTest.java
+++ src/net/sf/memoranda/tests/CalendarDateTest.java
@@ -1,4 +1,4 @@
-package test;
+package net.sf.memoranda.tests;
 
 
 import static org.junit.Assert.*;
