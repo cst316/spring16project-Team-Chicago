c9c8e7033088edd1d25f2736bc96920904e86596
Vikas Bhagat <vbhagat@asu.edu>
2016-02-22 17:44:47 -0600

    Updated groups checkstyle file Corrected style errors Corrected logic error

--- src/net/sf/memoranda/ui/ImportSticker.java
+++ src/net/sf/memoranda/ui/ImportSticker.java
@@ -1,79 +1,65 @@
 package net.sf.memoranda.ui;
 
-
-import javax.swing.SwingUtilities;
 import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
 import javax.swing.JFileChooser;
-import javax.swing.JScrollPane;
-
-import net.sf.memoranda.util.Local;
 import java.io.*;
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import net.sf.memoranda.CurrentProject;
-import net.sf.memoranda.EventNotificationListener;
 import net.sf.memoranda.EventsManager;
-import net.sf.memoranda.EventsScheduler;
-import net.sf.memoranda.History;
-import net.sf.memoranda.NoteList;
-import net.sf.memoranda.Project;
-import net.sf.memoranda.ProjectListener;
-import net.sf.memoranda.ProjectManager;
-import net.sf.memoranda.ResourcesList;
-import net.sf.memoranda.TaskList;
-import net.sf.memoranda.date.CalendarDate;
-import net.sf.memoranda.date.CurrentDate;
-import net.sf.memoranda.date.DateListener;
-import net.sf.memoranda.util.AgendaGenerator;
 import net.sf.memoranda.util.CurrentStorage;
 import net.sf.memoranda.util.Local;
-import net.sf.memoranda.util.Util;
-import nu.xom.Element;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
 
-/**ImportStricker class 
+
+/**ImportStricker class. 
  * 
  * Finds file and reads file then creates stickies
  */
 public class ImportSticker {
 
-	 private String name;        
-	 private JEditorPane viewer = new JEditorPane("text/html", "");
+	 private String _name;        
+	 private JEditorPane _viewer = new JEditorPane("text/html", "");
 	
+	/**Import Sticker Constructor. 
+	 * 
+	 * @pram x String 
+	*/	
 	public ImportSticker(String x) {
-		name = x;
+		_name = x;
 	}
 	
-	/**Import Sticker file creates stickies 
+	/**Import Sticker file creates stickies. 
 	 * 
-	 * @return boolean
-	 */
-    public boolean importFile(){
+	 * @return boolean 
+	*/
+	public boolean importFile(){
     	boolean result = true;
     	String current;
-    	JFileChooser fx = new JFileChooser(new java.io.File("."));
-        fx.setDialogTitle("Import Location");
+    	final JFileChooser fx = new JFileChooser(new java.io.File("."));
+    	fx.setDialogTitle("Import Location");
         fx.setFileFilter(new FileTypeFilter(".html","HTML File"));
-        int exportResult = fx.showSaveDialog(null);
+        final int exportResult = fx.showSaveDialog(null);
         if (exportResult == JFileChooser.APPROVE_OPTION){
-        	File fi = fx.getSelectedFile(); 
+        	final File fi = fx.getSelectedFile(); 
+        	final String tempLocation = fi.getPath();
         		try {
-        			String tempLocation = fi.getPath();
-                    System.out.println(tempLocation);
-                    BufferedReader br = new BufferedReader(new FileReader(tempLocation));
-                    while ((current = br.readLine()) != null) {
-                    EventsManager.createSticker(current, 1);
-                    CurrentStorage.get().storeEventsManager();
-                    }
-                         
-                } catch (IOException e) {
-                    	e.printStackTrace();
-                    	JOptionPane.showMessageDialog(null,Local.getString("Import Failed"));
-                   }	
-                    
+        			final BufferedReader  br = new BufferedReader(new FileReader(tempLocation));
+        			try {
+        				while ((current = br.readLine()) != null) {
+        				EventsManager.createSticker(current, 1);
+        				CurrentStorage.get().storeEventsManager();
+        				}
+        			}	
+        				finally {
+            				br.close();
+            			}
+                } 
+        		catch (IOException e) {
+                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(null,Local.getString("Import Failed"));
+                    result = false;
+                   }
+               
         }              
         return result;
     }
