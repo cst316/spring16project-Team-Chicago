2f110181055e24c07e1f8111be166836204a9c12
jdhinkle <jdhinkle@asu.edu>
2016-02-22 17:08:30 -0600

    Added javadoc comments to IllegalEmailException and IllegalPhoneNumberException classes

--- src/net/sf/memoranda/IllegalEmailException.java
+++ src/net/sf/memoranda/IllegalEmailException.java
@@ -1,4 +1,14 @@
+/**
+ * Exception class for illegal email format strings.
+ * @author Jonathan Hinkle
+ */
+
 package net.sf.memoranda;
 
+/**
+ * This class is thrown when a string is not a valid email format.
+ * @author Jonathan Hinkle
+ *
+ */
 @SuppressWarnings("serial")
 public class IllegalEmailException extends IllegalArgumentException {}
--- src/net/sf/memoranda/IllegalPhoneNumberException.java
+++ src/net/sf/memoranda/IllegalPhoneNumberException.java
@@ -1,5 +1,14 @@
+/**
+ * Exception class for illegal phone number format strings.
+ * @author Jonathan Hinkle
+ */
+
 package net.sf.memoranda;
 
-
+/**
+ * This class is thrown when a string is not a valid phone number format.
+ * @author Jonathan Hinkle
+ *
+ */
 @SuppressWarnings("serial")
 public class IllegalPhoneNumberException extends IllegalArgumentException {}
